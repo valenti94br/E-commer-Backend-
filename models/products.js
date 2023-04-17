2cd5ac742bba03718afd1c1b2edf71807c28c25e
valenti94br <valenti94br@gmail.com>
2023-04-17 22:45:51 +0200

    models corregidos

---
+++ models/products.js
@@ -0,0 +1,49 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Product extends Model {
+    static associate(models) {
+      // define association here
+      Product.hasMany(models.Review)
+      Product.belongsTo(models.Category)
+      Product.belongsToMany(models.Order, {
+        through: models.OrderProduct
+      })
+    }
+  }
+  Product.init({
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          message: 'Introduce el nombre del producto'
+        }
+      }
+    },
+    price: {
+      type: DataTypes.NUMERIC(10, 2),
+      allowNull: false,
+      validate: {
+        notNull: {
+          message: 'Introduce el precio del producto'
+        }
+      }
+    },
+    CategoryId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        notNull: {
+          message: 'Introduce el ID del producto'
+        }
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'Producto',
+  });
+  return Product;
+};
---
+++ models/users.js
@@ -0,0 +1,63 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    static associate(models) {
+      // define association here
+      User.hasMany(models.Review)
+      User.hasMany(models.Order)
+    }
+  }
+  User.init({
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          message: 'Introduce your name'
+        }
+      }
+    },
+
+    surname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          message: 'Introduce your surname'
+        }
+      }
+    },
+
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        notNull: {
+          message: 'Introduce tu correo electrónico'
+        },
+        isEmail: true
+      }
+    },
+    
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          message: 'Introduce tu contraseña'
+        }
+      }
+    },
+
+    role: DataTypes.STRING,
+    confirmed: DataTypes.BOOLEAN  
+  }, {
+    sequelize,
+    modelName: 'Usuario',
+  });
+  return User;
+};
