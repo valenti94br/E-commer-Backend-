02f43247e65423677092a47faf8dc8840224c80b
valenti94br <valenti94br@gmail.com>
2023-04-17 23:36:39 +0200

    middleware repasado

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -2,13 +2,11 @@ const { User, Token, Sequelize } = require('../models');
 const { Op } = Sequelize;
 const jwt = require('jsonwebtoken');
 const { jwt_secret } = require('../config/config.json')['development']
-
-// Middleware para autenticación de token
 const authentication = async (req, res, next) => {
     try {
-        const token = req.headers.authorization; // Se obtiene el token del encabezado de la solicitud
-        const payload = jwt.verify(token, jwt_secret); // Se verifica y decodifica el token usando la clave secreta
-        const user = await User.findByPk(payload.id); // Se busca al usuario en la base de datos usando el ID del payload del token
+        const token = req.headers.authorization;
+        const payload = jwt.verify(token, jwt_secret);
+        const user = await User.findByPk(payload.id);
         const tokenFound = await Token.findOne({
             where: {
                 [Op.and]: [
@@ -16,18 +14,15 @@ const authentication = async (req, res, next) => {
                     { token: token }
                 ]
             }
-        }); // Se busca el token en la base de datos
-
-        if (!tokenFound) { // Si el token no se encuentra en la base de datos, se devuelve un error de no autorizado
-            return res.status(401).send({ message: 'No estás autorizado' });
+        });
+        if (!tokenFound) {
+            return res.status(401).send({ message: 'No estas autorizado' });
         }
-
-        req.user = user; // Se agrega el usuario a la solicitud para que esté disponible en los controladores siguientes
-        next(); // Se llama a la función next para continuar con el siguiente middleware o controlador
+        req.user = user;
+        next();
     } catch (error) {
         console.log(error)
-        res.status(500).send({ error, message: 'Ha habido un problema con el token' });
+        res.status(500).send({ error, message: 'Ha habido un problema con el token' })
     }
 }
-
-module.exports = { authentication };
+module.exports = { authentication }
