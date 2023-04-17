d0ec7cee6e1f41c6100c4d8346778e386661ab13
valenti94br <valenti94br@gmail.com>
2023-04-17 23:55:43 +0200

    index solucionado

--- index.js
+++ index.js
@@ -3,16 +3,9 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 
-app.use(express.json());
-
-app.use('/categories',require('./routes/categories.js'))
+app.use('/users',require('./routes/users.js'))
 app.use('/orders',require('./routes/orders.js'))
 app.use('/products',require('./routes/products.js'))
-app.use('/users',require('./routes/users.js'))
+app.use('/categories',require('./routes/categories.js'))
 
-
-
-app.listen(PORT, () => {
-    console.log(`Servidor levantado en el puerto ${PORT}`);
-});
-
+    .listen(PORT,()=>console.log(`Servidor levantado correctamente en el puerto ${PORT}`))
