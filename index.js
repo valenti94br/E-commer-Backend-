8ac2463e789bf5e4714565d328888f4c82558569
valenti94br <valenti94br@gmail.com>
2023-04-17 22:13:42 +0200

    como soy un neuras, he ordenado los app.use para que estén en el mismo orden que los controllers. Me daba tock

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ app.use(express.json())
 
 app.use(express.json());
 
-app.use('/users',require('./routes/users.js'))
+app.use('/categories',require('./routes/categories.js'))
 app.use('/orders',require('./routes/orders.js'))
 app.use('/products',require('./routes/products.js'))
-app.use('/categories',require('./routes/categories.js'))
+app.use('/users',require('./routes/users.js'))
+
+
 
 app.listen(PORT, () => {
     console.log(`Servidor levantado en el puerto ${PORT}`);
